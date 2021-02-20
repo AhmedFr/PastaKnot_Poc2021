aca84ed318e1259df49b9959023720039200db20
baptiste.barbotin@epitech.eu <baptiste.barbotin@epitech.eu>
2021-02-20 13:48:47 +0000

    modfication of structure file

--- front/pastaknot/src/index.tsx
+++ front/pastaknot/src/index.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./index.css";
+import { Home } from "./components/Home/home";
 
 ReactDOM.render(
   <React.StrictMode>
-
-    
-  </React.StrictMode>,
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/">
+              <Home/>
+            </Route>
+        </Switch>
+    </BrowserRouter>
+</React.StrictMode>,
   document.getElementById("root")
 );
