8c71f626e209c04807d27f1f3c86bb44797f29a9
jules.colas@epitech.eu <jules.colas@epitech.eu>
2021-02-20 05:30:41 -0500

    prototype post et get pour l'api

--- back/server.ts
+++ back/server.ts
@@ -3,29 +3,90 @@ import cookieParser from 'cookie-parser'
 import bodyParser from  'body-parser'
 
 var server = express();
+server.use(bodyParser.json())
+server.use(cookieParser());
+
 
 server.get("/Pastaknot/home", function(request, response) {
-
+  // get si il est connecter, les catégories, nb de tips? de la database
 });
 
 server.get("Pastaknot/50nuancedepasta", function(request, response) {
+  // get si il est connecter, les tips, nb de like, nb de comment de la db
+});
+server.post("Pastaknot/50nuancedepasta", function(request, response) {
+  if (!Object(request.body).length) {
+    response.status(400);
+    response.send("Bad request");
+  }
+  else {
+    //mettre dans la db en fonction du type de la requête (comment ou tip)
+  }
+});
+
+server.get("/Pastaknot/lesPlatsduRadin", function(request, response) {
+  // get si il est connecter, les tips, nb de like, nb de comment de la db
+});
+
+server.post("Pastaknot/lesPlatsduRadin", function(request, response) {
+  if (!Object(request.body).length) {
+    response.status(400);
+    response.send("Bad request");
+  }
+  else {
+    //mettre dans la db en fonction du type de la requête (comment ou tip)
+  }
+});
+
+server.get("/Pastaknot/antiBidesaBiere", function(request, response) {
+  // get si il est connecter, les tips, nb de like, nb de comment de la db
+});
+
+server.post("Pastaknot/antibidesaBiere", function(request, response) {
+  if (!Object(request.body).length) {
+    response.status(400);
+    response.send("Bad request");
+  }
+  else {
+    //mettre dans la db en fonction du type de la requête (comment ou tip)
+  }
+});
+
+server.get("/Pastaknot/raptaQuirhallas", function(request, response) {
+  // get si il est connecter, les tips, nb de like, nb de comment de la db
 
 });
 
-server.get("Pastaknot/lesPlatsduRadin", function(request, response) {
-
+server.post("Pastaknot/raptaQuirhallas", function(request, response) {
+  if (!Object(request.body).length) {
+    response.status(400);
+    response.send("Bad request");
+  }
+  else {
+    //mettre dans la db en fonction du type de la requête (comment ou tip)
+  }
 });
 
-server.get("Pastaknot/antiBidesaBiere", function(request, response) {
-
+server.get("/Pastaknot/login", function(request, response) {
+  if (!Object(request.body).length) {
+    response.status(400);
+    response.send("Bad request");
+  }
+  else {
+    //comparer le resultat du body password + body username avec la data base
+    //si c'est vrai, le connecter.
+    //si c'est faux status 400 et message "mauvais mdp ou username"
+  }
 });
 
-server.get("Pastaknot/raptaQuirhallas", function(request, response) {
-
-});
-
-server.get("Pastaknot/login", function(request, response) {
-
+server.post("/Pastaknot/register", function(request, response) {
+  if (!Object(request.body).length) {
+    response.status(400);
+    response.send("Bad request");
+  }
+  else {
+    //new in database with request.body.password and request.body.username
+  }
 });
 
 server.listen(8080);
