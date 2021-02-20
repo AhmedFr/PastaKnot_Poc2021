d3d19ee76b50fc7c98398f7fd01ceeec50da6b88
Slooth <victor.guyot@epitech.eu>
2021-02-20 19:16:30 +0100

    mainpage darktheme

--- front/pastaknot/src/components/Home/cate_item.tsx
+++ front/pastaknot/src/components/Home/cate_item.tsx
@@ -3,10 +3,12 @@ import "./home.css";
 
 export function Cate_Item(props: any): JSX.Element{
     return(
+        <a href={props.link} className="itemlink">
         <div className="item" id="Pasta">
         <img src={props.image}></img>
         <h1>
             {props.messageText}
         </h1>
-        </div>);
+        </div>
+        </a>);
 }
